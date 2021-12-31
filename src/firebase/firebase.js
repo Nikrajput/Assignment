95a98e0659e9882f77a968e75eb1d7eb270c7277
Nikhil Kumar <81246726+Nikrajput@users.noreply.github.com>
2021-12-31 15:10:29 +0530

    added env file

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -8,21 +8,21 @@ import {
 import { getDatabase } from "firebase/database";
 import { getStorage } from "firebase/storage";
 
-const config = {
-    apiKey: "AIzaSyCqPBOGuyXsZ0aFvgJbjnox8Gaos4jUmWU",
-    authDomain: "assignment-c637d.firebaseapp.com",
-    databaseURL: "https://assignment-c637d-default-rtdb.firebaseio.com",
-    projectId: "assignment-c637d",
-    storageBucket: "assignment-c637d.appspot.com",
-    messagingSenderId: "261391711922",
-    appId: "1:261391711922:web:1ded6ad53d4cff521453ab"
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 if (!firebase.apps.length) {
   //initializing with the config object
-  firebase.initializeApp(config);
+  firebase.initializeApp(firebaseConfig);
 }
 
-const firebaseApp = initializeApp(config);
+const firebaseApp = initializeApp(firebaseConfig);
 
 // ** Modulerized Firebase ** //
 const auth = getAuth(firebaseApp);
